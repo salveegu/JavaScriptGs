6531b9e3790e1bf43bd2db95ebeb0ae1fbad838d
Gustavo Souza <salveegu@gmail.com>
2022-02-23 11:02:44 -0300

    11:02 quarta-feira, 23 de fevereiro de 2022 (BRT) Horário em São Mateus, São Paulo - SP

---
+++ js001/JogoDaVelha.html/js/script.js
@@ -0,0 +1,18 @@
+let x = document.querySelector('.x')
+let o = document.querySelector('.o')
+let boxes = document.querySelectorAll('.box')
+let buttons = document.querySelectorAll('#buttons-container button')
+let messageContainer = document.querySelector('#message')
+let messageText = document.querySelector('#message p')
+let secondPlayer
+
+//contador de jogadas
+
+let player1=0
+let player2=0
+
+//adicionando o evento de click aos boxes
+
+for (let i = 0;i< boxes.length;i++){
+
+}
